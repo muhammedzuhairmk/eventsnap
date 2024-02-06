3500a61e4edde5ca8b4a6d17e6610948c77aa070
MUHAMMED SUHAIR MK <125701049+muhammedzuhairmk@users.noreply.github.com>
2024-02-06 11:24:48 +0530

    error

--- src/routes/userRouter.js
+++ src/routes/userRouter.js
@@ -11,6 +11,7 @@ const storage = multer.diskStorage({
         cb(null, "public/images/users");
     },
     filename: function (req, file, cb) {
+        
         const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
         const ext = path.extname(file.originalname);
         cb(null, file.fieldname + "-" + uniqueSuffix + ext);
